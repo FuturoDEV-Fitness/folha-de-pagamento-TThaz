7c071c5a6c9fe81c0da6da5d4df9198d67fe45f3
Thalles Azevedo <TThaz>
2024-05-18 09:24:12 -0300

    Resolução exercício 5

--- folha_pagamento/src/calculo_salario_liquido.js
+++ folha_pagamento/src/calculo_salario_liquido.js
@@ -1,21 +1,12 @@
 const calcularINSS = require("./calculo_inss");
 const calcularImpostoRenda = require("./calculo_imposto_renda")
 
-
-
 function calcSalarioLiquido(salario) {
 
     let descontoINSS = Number(calcularINSS(salario))
     let descontoIR = Number(calcularImpostoRenda(salario))
 
-    console.log(`Salário inicial: ${salario}`)
-    console.log(`Desconto INSS: ${descontoINSS}`)
-    console.log(`Desconto IR: ${descontoIR}`)
-
     let salarioLiquido = (salario - (descontoINSS + descontoIR)).toFixed(2)
-    // console.log(salarioLiquido)
-
-    console.log(`Salário liquido: ${salarioLiquido}`)
     return salarioLiquido
 }
 
